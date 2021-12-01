87bb18ca807a11e7f9cf75903cf23f515ae56fec
Adriaon Righi <contato@adrianorighi.com>
2021-11-30 21:17:30 -0300

    chore: create dockerfile for checks-service package

--- packages/checks-service/src/services/telegram.js
+++ packages/checks-service/src/services/telegram.js
@@ -1,6 +1,5 @@
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
-const firestore = require('./firestore');
 const { save, remove, count, find } = require('../database/mongodb');
 const { token } = require('../configs/telegram');
 
